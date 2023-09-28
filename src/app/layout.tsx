5b32b77af4e5379e621cbe3d7f56a44079cf7f96
Andrey <andreyevyk13@gmail.com>
2023-09-28 01:00:18 -0300

    chore: config eslint, prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,11 +9,7 @@ export const metadata: Metadata = {
   description: 'Plataforma de adoçõa de pets',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>{children}</body>
