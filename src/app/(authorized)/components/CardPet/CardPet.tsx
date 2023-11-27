5c3d2557641aabb9ce33f1065ef1dd4bd056fc5f
Kennedy-Anderson <kennedyandrade98@gmail.com>
2023-11-27 19:27:33 -0300

    Exibe os cards ná página de busca de pets

--- src/app/(authorized)/components/CardPet/CardPet.tsx
+++ src/app/(authorized)/components/CardPet/CardPet.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { variants } from './CardPet.styles'
 
 interface CardPetProps {
   name: string;
@@ -6,8 +7,14 @@ interface CardPetProps {
 }
 
 export const CardPet: React.FC<CardPetProps> = ({ name, imageUrl }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleCardClick = () => {
+    setIsSelected(!isSelected);
+  };
+  
   return (
-    <div className="flex flex-col items-center h-56 w-56 px-4 pt-5 pb-0 bg-white border border-solid border-gray-100 rounded-2xl shadow-card">
+    <div className={variants({ selected: isSelected })} onClick={handleCardClick}>
       <h2 className="text-base font-sans text-center text-gray-800 font-semibold">{name}</h2>
       <img src={imageUrl} alt={name} className="w-36 h-36 rounded-md bottom-0" />
     </div>
