1ed91432e94d79398447700b6d920bea3263e43b
djairmaykon <djairmaykon@gmail.com>
2023-11-30 20:01:33 -0300

    feat: update user

---
+++ src/app/(authorized)/hooks/useUserUpdate.tsx
@@ -0,0 +1,25 @@
+import api from '@/core/api/api'
+
+export interface UpdateUserRequest {
+  id: number
+  name?: string
+  email?: string
+  password?: string
+}
+
+export const useUserUpdate = () => {
+  const updateUser = async (user: UpdateUserRequest) => {
+    const response = await api.patch<Omit<UpdateUserRequest, 'id'>>(
+      '/user/update/1',
+      {
+        name: user.name,
+        email: user.email,
+      },
+    )
+    console.log(response)
+
+    return response.data
+  }
+
+  return { updateUser }
+}
