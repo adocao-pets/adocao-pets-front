c9dfb130fcf07ed5ee5075432775444721c80913
djairmaykon <djairmaykon@gmail.com>
2023-11-30 16:52:35 -0300

    feat: login

--- src/app/(authorized)/pet-create/page.tsx
+++ src/app/(authorized)/pet-create/page.tsx
@@ -1,7 +1,15 @@
+'use client'
 import { Header } from '@/components/Header'
 import { CreatePetForm } from './CreatePetForm'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function PetCreatePage() {
+  const { replace } = useRouter()
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+    if (!user) replace('/login')
+  }, [])
   return (
     <>
       <Header title="Cadastrar Pet" />
--- src/app/(authorized)/user-edit/page.tsx
+++ src/app/(authorized)/user-edit/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
 import { Header } from '@/components/Header'
 import { UserEditForm } from './UserEditForm'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function UserEditPage() {
+  const { replace } = useRouter()
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+    if (!user) replace('/login')
+  }, [])
+
   return (
     <>
       <Header title="Editar usuário" />
